65622a2517e6ec036852a495a0e6e1226a75de23
NikolaVk <nikola@ique.nl>
2022-01-01 21:27:17 +0000

    finish testing

--- todo/test_forms.py
+++ todo/test_forms.py
@@ -8,7 +8,7 @@ class TestItemForm(TestCase):
         form = ItemForm({'name': ''})
         self.assertFalse(form.is_valid())
         self.assertIn('name', form.errors.keys())
-        self.assertEqual(form.errors['name'][0], 'This field is required')
+        self.assertEqual(form.errors['name'][0], 'This field is required.')
 
     def test_done_field_is_not_required(self):
         form = ItemForm({'name': 'Test Todo Item'})
@@ -16,4 +16,4 @@ class TestItemForm(TestCase):
 
     def test_fields_are_explicit_in_form_metaclass(self):
         form = ItemForm()
-        self.assertEqual(['name', 'done'])
+        self.assertEqual(form.Meta.fields, ['name', 'done'])
